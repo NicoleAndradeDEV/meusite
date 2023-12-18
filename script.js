784d582a6f0f6758c6606932cbbe4064ff910cda
Nicole Andrade <144156077+NicoleAndradeDEV@users.noreply.github.com>
2023-12-18 09:17:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Simulando a funcionalidade de login
+document.getElementById('interesse-btn').addEventListener('click', function() {
+    alert('Clique em meus trabalhos para descobrir o que sei fazer e depois em Contato para me contratar.');
+});
+
+// Simulando a exibição sobre mim
+document.getElementById('news-container').innerHTML = '<h2>Quer descobrir o básico sobre mim? </h2><p>Tenho 24 anos, sou estudante e estágiaria. Atuo na area de Tecnologia e estou no quarto semestre de Gestão da Tecnologia da Informação. </p>';
